ab131d89af8017e3e31e4ffb35c04a53adb8d862
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-10-28 18:31:44 -0700

    Added Redux to Products Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './containers/HomeScreen';
+import ProductScreen from './containers/ProductScreen';
 
 const App = () => {
   return (
-    <div className="App">
-      <>
-      <h1>Welcome To ProShop</h1>
-      </>
-    </div>
+      
+        <Router>
+          <Header />
+        <main className='py-3' >
+          <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+          <Footer />
+        </Router>
+      
   );
 }
 
